6032cd5831d71b16567b1927c16f2c3d564a49ec
alexandrgur81 <50299695+alexandrgur81@users.noreply.github.com>
2019-07-03 15:45:14 +0300

    защита 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
-var link = document.querySelector(".appointment-search-btn");
-var popup = document.querySelector(".appointment-form");
+var block=document.querySelector(".appointment-search-btn");
+var popup=document.querySelector(".hide-form");
+var form=popup.querySelector(".appointment-form");
+var arrival=popup.querySelector("[name=data-arrival]");
+var departure=popup.querySelector("[name=data-departure]");
+var adults=popup.querySelector("[name=adults]");
+var children=popup.querySelector("[name=children]");
+var storage=localStorage.getItem("adults");
+var number=localStorage.getItem("children");
 
-link.addEventListener("click", function (evt) {
-  popup.classList.toggle("appointment-form-show");
+block.addEventListener("click", function (evt) {
+  popup.classList.toggle("visual");
+  if (storage && number) {
+    adults.value=storage;
+    children.value=number;
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value || !adults.value || !children.value ) {
+    evt.preventDefault();
+    console.log("Не хватает данных");
+  } else {
+    localStorage.setItem("adults", adults.value);
+    localStorage.setItem("children", children.value);
+  }
 });
